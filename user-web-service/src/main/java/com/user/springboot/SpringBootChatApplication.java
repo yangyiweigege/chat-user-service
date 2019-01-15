5e2c834c8945cf53da562034c7fc75c190c7bfca
yangxiaowei <2873824885@qq.com>
2019-01-15 20:33:14 +0800

    暂时注释了分布式代码

--- user-web-service/src/main/java/com/user/springboot/SpringBootChatApplication.java
+++ user-web-service/src/main/java/com/user/springboot/SpringBootChatApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.netflix.feign.EnableFeignClients;
 //import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -21,7 +21,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication(exclude = { HibernateJpaAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class }) // spring-boot																										// 启动类
 @ComponentScan(basePackages = { "com.user.springboot" }) // 基本扫包配置
 @MapperScan("com.user.springboot.dao") // 配置mybatis-dao层扫描
-@EnableEurekaClient // 服务注册 
+//@EnableEurekaClient // 服务注册
 @ServletComponentScan // servlete组件扫描
 @EnableScheduling // 定时任务配置开启
 // @EnableDiscoveryClient//发现服务
